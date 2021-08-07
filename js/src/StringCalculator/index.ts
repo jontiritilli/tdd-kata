3f23a6332002cedd9d4da127d7bf0a7450705cd6
jontiritilli <jonathantiritilli@gmail.com>
2021-08-07 01:41:48 -0700

    allow custom delimiters

--- js/src/StringCalculator/index.ts
+++ js/src/StringCalculator/index.ts
@@ -10,19 +10,30 @@ export class StringCalculator {
             return parseInt(input)
 
         const numbers = this.GetNumbersArray(input)
+        
         this.ValidateNumbers(numbers)
         return numbers.reduce((acc: number, number: number): number => {
             return acc += number
             }, 0)
     }
     GetNumbersArray(input: string): number[] {
-        input = input.replace('\n', ',')
-        const numbers = input.split(',').map(num => parseInt(num))
+        const sanitizedInput = this.ReplaceDelimiters(input)
+
+        const numbers = sanitizedInput.split(',').map(num => parseInt(num))
         return numbers.filter(num => num <= 1000)
     }
     ValidateNumbers(input: number[]): void {
         const negativeNumbers = input.filter(num => num < 0)
         if (negativeNumbers.length)
             throw new Error('illegal characters. ensure all values are positive integers')
+    }
+    ReplaceDelimiters(input: string): string {
+        const isCustomDelimiter = input.startsWith('//')
+        console.log({isCustomDelimiter})
+        if (isCustomDelimiter) {
+            const delimiter = input.slice(2, input.indexOf('\n'))
+            return input.substr(3 + delimiter.length).replace(delimiter, ',')
+        }
+        return input.replace('\n', ',')
     }
 }
