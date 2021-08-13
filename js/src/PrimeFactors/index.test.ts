5f936fe92d6c5ed9795bb982e42f77db80fa8ec9
jontiritilli <jonathantiritilli@gmail.com>
2021-08-13 09:57:18 -0700

    should return [2,3] for 6

--- js/src/PrimeFactors/index.test.ts
+++ js/src/PrimeFactors/index.test.ts
@@ -13,22 +13,22 @@ describe('prime factor generator',  () => {
     it('should return [2,2] for 4', () => {
         expect(getPrimeFactors(4)).toEqual([2,2])
     })
-    it('it should return [5] for 5', ()=>{
+    it('should return [5] for 5', ()=>{
         expect(getPrimeFactors(5)).toEqual([5])
     })
-    // it('it should return [2,3] for 6', ()=>{
-    //     expect(getPrimeFactors(6)).toEqual([2,3])
-    // })
-    // it('it should return [7] for 7', ()=>{
+    it('should return [2,3] for 6', ()=>{
+        expect(getPrimeFactors(6)).toEqual([2,3])
+    })
+    // it('should return [7] for 7', ()=>{
     //     expect(getPrimeFactors(7)).toEqual([7])
     // })
-    // it('it should return [2,2,2] for 8', ()=>{
+    // it('should return [2,2,2] for 8', ()=>{
     //     expect(getPrimeFactors(8)).toEqual([2,2,2])
     // })
-    // it('it should return [3,3] for 9', ()=>{
+    // it('should return [3,3] for 9', ()=>{
     //     expect(getPrimeFactors(9)).toEqual([3,3])
     // })
-    // it('it should return [2,2,3,5,7,11] for 4620', ()=>{
+    // it('should return [2,2,3,5,7,11] for 4620', ()=>{
     //     expect(getPrimeFactors(4620)).toEqual([2,2,3,5,7,11])
     // })
 })
