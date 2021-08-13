819fde104a7ca7e2684eeb9619017dbdba441cca
jontiritilli <jonathantiritilli@gmail.com>
2021-08-13 09:11:24 -0700

    should return [2] for 2

--- js/src/PrimeFactors/index.test.ts
+++ js/src/PrimeFactors/index.test.ts
@@ -5,4 +5,8 @@ describe('prime factor generator',  () => {
         const result = generate(1)
         expect(result).toEqual([])
     })
+    it('should return [2] for 2', () => {
+        const result = generate(2)
+        expect(result).toEqual([2])
+    })
 })
--- js/src/PrimeFactors/index.ts
+++ js/src/PrimeFactors/index.ts
@@ -1,4 +1,7 @@
 export const generate = (input: number): number[] => {
     if (input < 2) 
         return []
+    if (input % 2 == 0) {
+        return [input]
+    }
 }
